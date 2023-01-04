f6081fff7dc8886f7149e92052d994bdde23a6cc
ChaimaeBinjach <binjachchaimaa@gmail.com>
2023-01-04 18:26:29 +0100

    hi

--- virtual_reality/userinformation.php
+++ virtual_reality/userinformation.php
@@ -1,17 +1,15 @@
 <?php
 
-$connection = mysqli_connect('localhost:3306','alimehbo_ali','alimehboob@1234','alimehbo_phpwebsite');
+$connection = mysqli_connect('localhost', 'root');
 
-mysqli_select_db($connection,"youtubedata");
+mysqli_select_db($connection, "virtual");
 
 $user = $_POST['user'];
 $email = $_POST['email'];
 $message = $_POST['message'];
 
-$query = "INSERT INTO `userinfodata`(`user`,`email`,`message`) VALUES ('$user','$email','$message') ";
+$query = "INSERT INTO `data`(`user`,`email`,`message`) VALUES ('$user','$email','$message') ";
 
-mysqli_query($connection,$query);
+mysqli_query($connection, $query);
 
-echo "MESSAGE IS SENT";
-
-?>
+echo "well done :)";
