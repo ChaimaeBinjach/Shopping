085de732fdfc23c12105e1be772fd5666b4be04f
ChaimaeBinjach <binjachchaimaa@gmail.com>
2023-01-13 20:44:59 +0100

    shiny

--- Shopping/admin_products.php
+++ Shopping/admin_products.php
@@ -9,23 +9,25 @@
 
 //     header('location:login.php');
 // }
+
 include 'config.php';
 
-if (session_status() == PHP_SESSION_NONE) {
+if (!isset($_SESSION)) {
     session_start();
 }
-$admin_id = $_SESSION['admin_id'];
-if (!$admin_id) {
+
+if (!isset($_SESSION['admin_id'])) {
     header('location: login.php');
     exit;
 }
+
 if (isset($_POST['add_product'])) {
     $name = mysqli_real_escape_string($conn, $_POST['name']);
     $price = $_POST['price'];
     $image = $_FILES['image']['name'];
     $image_size = $_FILES['image']['size'];
     $image_tmp_name = $_FILES['image']['tmp_name'];
-    $image_folder = 'img_uploaded/' . $image;
+    $image_folder = $_SERVER['DOCUMENT_ROOT'] . '/Shopping/img_uploaded/' . $image;
 
     $select_product_name = mysqli_query($conn, "SELECT * FROM `products` WHERE name ='$name'") or die('query failed');
 
@@ -38,14 +40,23 @@ if (isset($_POST['add_product'])) {
             if ($image_size > 200000) {
                 $message[] = 'image size is too big';
             } else {
-                move_uploaded_file($image_tmp_name, $image_folder);
-                $message[] = 'Produced added! Perfect.';
+                if (move_uploaded_file($image_tmp_name, $image_folder)) {
+                    $message[] = 'Produced added! Perfect.';
+                } else {
+                    $message[] = 'error while uploading image';
+                }
             }
         } else {
             $message[] = 'Unfortunately.. The product is not added!';
         }
     }
 }
+if (isset($_GET['delete'])) {
+    $delete_id = $_GET['delete'];
+    mysqli_query($conn, "DELETE FROM `products` WHERE id ='$delete_id'") or die('query failed');
+    header('location:admin_products.php');
+}
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -80,11 +91,71 @@ if (isset($_POST['add_product'])) {
             <input type="file" name="image" accept="image/jpg, image/png, image/jpeg" class="box" required>
             <input type="submit" value="add product" name="add_product" class="sub">
         </form>
+
+
     </secttion>
 
     <!-- product crud section ends -->
 
+    <!-- show products -->
+    <section class="show-products">
 
+        <div class="box-container">
+
+            <?php
+            $select_products = mysqli_query($conn, "SELECT * FROM `products`") or die('query failed');
+            if (mysqli_num_rows($select_products) > 0) {
+                while ($fetch_products = mysqli_fetch_assoc($select_products)) {
+            ?>
+                    <div class="box">
+                        <img src="img_uploaded/<?php echo $fetch_products['image']; ?>" alt="">
+                        <div class="name"><?php echo $fetch_products['name']; ?></div>
+                        <div class="price">$<?php echo $fetch_products['price']; ?>/-</div>
+                        <a href="admin_products.php?update=<?php echo $fetch_products['id']; ?>" class="option-sub">update</a>
+                        <a href="admin_products.php?delete=<?php echo $fetch_products['id']; ?>" class="delete-sub" onclick="return confirm('are you sure you want to delete it?');">delete</a>
+                    </div>
+            <?php
+                }
+            } else {
+                echo '<p class="empty">no products added yet!</p>';
+            }
+            ?>
+        </div>
+    </section>
+
+    <section class="edit-product-form">
+
+        <?php
+        if (isset($_GET['update'])) {
+            $update_id = mysqli_real_escape_string($conn, $_GET['update']);
+            $update_query = "SELECT * FROM `products` WHERE id = '$update_id'";
+            $result = mysqli_query($conn, $update_query);
+
+            if (!$result) {
+                die("query failed: " . mysqli_error($conn));
+            } elseif (mysqli_num_rows($result) > 0) {
+                while ($fetch_update = mysqli_fetch_assoc($result)) {
+        ?>
+                    <form action="" method="post" enctype="multipart/form-data">
+                        <input type="hidden" name="update_p_id" value="<?php echo $fetch_update['id']; ?>">
+                        <input type="hidden" name="update_old_image" value="<?php echo $fetch_update['image']; ?>">
+                        <img src="img_uploaded/<?php echo $fetch_update['image']; ?>" alt="">
+                        <input type="text" name="update_name" value="<?php echo $fetch_update['name']; ?>" class="box" required placeholder="Enter product name">
+                        <input type="number" name="update_price" value="<?php echo $fetch_update['price']; ?>" min="0" class="box" required placeholder="Enter product price">
+                        <input type="file" class="box" name="update_image" accept="image/jpg, image/jpeg, image/png">
+                        <input type="submit" value="update" name="update_product" class="sub">
+                        <input type="reset" value="cancel" id="close-update" class="option-sub">
+                    </form>
+        <?php
+                }
+            }
+        } else {
+            echo '<script>document.querySelector(".edit-product-form").style.display = "none";</script>';
+        }
+        ?>
+
+
+    </section>
 
 
 
--- Shopping/javas/admin_script.js
+++ Shopping/javas/admin_script.js
@@ -1,50 +1,25 @@
+let navbar = document.querySelector('.header .navbar');
+let accountBox = document.querySelector('.header .account-box');
 
-// let navbar = document.querySelector('.header .navbar');
-// let accoutBox = document.querySelector('.header .account-box');
-
-// document.querySelector('#menubtn').onclick =()=>{
-//     navbar.classList.toggle('active');
-//     accoutBox.classList.tremove('active');
-// }
-// document.querySelector('#userbtn').onclick =()=>{
-//     accoutBox.classList.toggle('active');
-//     navbar.classList.remove('active');
-    
-// }
-
-// window.onscroll =()=>{
-//     navbar.classList.remove('active');
-//     accoutBox.classList.tremove('active');
-// }
-
-const navbar = document.querySelector('.header .navbar');
-const accountBox = document.querySelector('.header .account-box');
-const menuButton = document.querySelector('#menubtn');
-const userButton = document.querySelector('#userbtn');
-
-// toggleActive() function that takes an element as a parameter and toggles the "active" class on it.
-function toggleActive(element) {
-  element.classList.toggle('active');
+document.querySelector('#menubtn').onclick = () =>{
+   navbar.classList.toggle('active');
+   accountBox.classList.remove('active');
 }
 
-//removeActive() function that takes an element as a parameter and removes the "active" class from it.
-function removeActive(element) {
-  element.classList.remove('active');
+document.querySelector('#userbtn').onclick = () =>{
+   accountBox.classList.toggle('active');
+   navbar.classList.remove('active');
 }
 
-//onclick event listeners with addEventListener() and passed in the click event as the first argument.
-menuButton.addEventListener('click', () => {
-  toggleActive(navbar);
-  removeActive(accountBox);
-});
+window.onscroll = () =>{
+   navbar.classList.remove('active');
+   accountBox.classList.remove('active');
+}
 
-//onscroll event listener with addEventListener() and passed in the scroll event as the first argument.
-userButton.addEventListener('click', () => {
-  toggleActive(accountBox);
-  removeActive(navbar);
-});
+document.querySelector('#option-sub').onclick = () =>{
+  document.querySelector('.edit-product-form').style.display = 'none';
+  window.location.href = 'admin_products.php';
+}
 
-window.addEventListener('scroll', () => {
-  removeActive(navbar);
-  removeActive(accountBox);
-});
+
+
